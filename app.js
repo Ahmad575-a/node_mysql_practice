990714707b281606b2a7c801906a14c38a8a453f
Ahmad575-a <ahmad.alghetheth@gmail.com>
2021-07-10 16:10:42 +0200

    created a post table

--- app.js
+++ app.js
@@ -4,28 +4,39 @@ const app = express()
 
 // create a connection and connect it 
 const db = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'nodemysql'
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nodemysql'
 });
 
-db.connect((err)=>{
-    if(err) throw err;
+db.connect((err) => {
+    if (err) throw err;
     console.log('mysql is connected successfully...');
- });
+});
 
- // create DB
-app.get('/createdb', (req,res) =>{
+// create DB
+app.get('/createdb', (req, res) => {
     let sql = 'CREATE DATABASE nodemysql';
-    db.query(sql, (err,result) => {
-        if(err) throw err;
+    db.query(sql, (err, result) => {
+        if (err) throw err;
         console.log(result);
         res.send('database created successfully...');
     })
 })
- 
+
+// create Table 
+
+app.get('/createPostTable', (req, res) => {
+    let sql = 'CREATE TABLE Posts (id int Auto_increment primary key,title varchar(255), body varchar(255))';
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('posts table created successfully..');
+    })
+})
 
 
 
-app.listen(process.env.PORT || 5000, ()=> console.log('server is running'));
+
+app.listen(process.env.PORT || 5000, () => console.log('server is running'));
